6efe965177131a4a3219b054607fa6c7a889c2f4
Timo Riski <25483483+rriski@users.noreply.github.com>
2021-12-21 23:09:14 +0200

    ssh: remove UseKeychain

--- ssh/install.fish
+++ ssh/install.fish
@@ -7,10 +7,5 @@ test -f ~/.ssh/config || touch ~/.ssh/config
 grep -q "Include ~/.ssh/config.dotfiles" ~/.ssh/config ||
 	echo "Include ~/.ssh/config.dotfiles" >> ~/.ssh/config
 
-switch (uname)
-case Darwin
-	grep -q "UseKeychain yes" ~/.ssh/config || echo -e '\nHost *\n  UseKeychain yes\n' >> ~/.ssh/config
-end
-
 link_file $DOTFILES_ROOT/ssh/config.dotfiles $HOME/.ssh/config.dotfiles backup
 	or abort ssh
