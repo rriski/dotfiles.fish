7785626792bcc06980d0e5ce1f2b82cc95f5777f
Timo Riski <25483483+rriski@users.noreply.github.com>
2022-03-08 15:16:21 +0200

    alacritty: add dracula theme

--- alacritty/install.fish
+++ alacritty/install.fish
@@ -2,7 +2,7 @@
 
 source $DOTFILES/script/utils.fish
 
-curl -sL https://raw.githubusercontent.com/dracula/alacritty/master/dracula.yml -o $DOTFILES/alacritty/dracula.yml
+curl -sL https://raw.githubusercontent.com/dracula/alacritty/master/dracula.yml -o ~/.config/alacritty/dracula.yml
 
 link_file $DOTFILES_ROOT/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml backup
 	or abort alacritty
