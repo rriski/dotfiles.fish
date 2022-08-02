2ccf0c680a2e113915355b04fbefe7be8bb0b1a9
Timo Riski <25483483+rriski@users.noreply.github.com>
2022-08-02 12:32:57 +0300

    feat(git): add .git-templates dir and hooks support

--- git/install.fish
+++ git/install.fish
@@ -4,3 +4,12 @@ if command -qs gh
 	abbr -a 'grv' 'gh repo view -w'
 	abbr -a 'gpv' 'gh pr view -w'
 end
+
+set TEMPLATES_DIR "$HOME/.git-templates/hooks"
+if ! test -d "$TEMPLATES_DIR"
+	mkdir -p "$TEMPLATES_DIR"
+end
+
+for hook in $DOTFILES/git/.git-templates/*
+	ln -sf $hook $TEMPLATES_DIR/(basename $hook)
+end
