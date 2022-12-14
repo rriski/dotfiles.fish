14b0b7e0db92d2be1452e55f376b10539066deb2
Timo Riski <25483483+rriski@users.noreply.github.com>
2022-12-14 10:24:36 +0200

    fix(aws): make AWS cli optional

--- aws/conf.d/aws.fish
+++ aws/conf.d/aws.fish
@@ -1,7 +1,7 @@
 if status is-interactive
-    if type -q aws_completer
-		complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
-	else
-		echo 'You need to install AWS CLI'
+	if command -qs aws
+		if type -q aws_completer
+			complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
+		end
 	end
 end
