c6f75b17388d78a53d499200ec30024351e9d4ed
Timo Riski <github@timoriski.fi>
2023-01-28 13:30:47 +0200

    git: split config into personal and work configs

--- script/utils.fish
+++ script/utils.fish
@@ -27,22 +27,7 @@ end
 
 function setup_gitconfig
 	set managed (git config --get dotfiles.managed)
-	# if there is no user.email, we'll assume it's a new machine/setup and ask it
-	if test -z (git config --get user.email)
-		user 'What is your git author name?'
-		read user_name
-		user 'What is your git author email?'
-		read user_email
-
-		test -n $user_name
-			or echo "please inform the git author name"
-		test -n $user_email
-			or abort "please inform the git author email"
-
-		git config --global user.name $user_name
-			and git config --global user.email $user_email
-			or abort 'failed to setup git user name and email'
-	else if test '$managed' = "true"
+	if test '$managed' = "true"
 		# if user.email exists, let's check for dotfiles.managed config. If it is
 		# not true, we'll backup the gitconfig file and set previous user.email and
 		# user.name in the new one
