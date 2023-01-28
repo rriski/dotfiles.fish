cc6ca2c0bbd796ab892265ed1d3729fa41f2c54a
Timo Riski <github@timoriski.fi>
2023-01-28 13:32:07 +0200

    fisher: add settings for `budimanjojo/tmux.fish`

---
+++ fisher/install.fish
@@ -0,0 +1,8 @@
+#!/usr/bin/env fish
+
+# budimanjojo/tmux.fish
+set -U fish_tmux_autostart true
+set -U fish_tmux_autostart_once true
+set -U fish_tmux_unicode true
+set -U fish_tmux_default_session_name main
+set -U fish_tmux_fixterm_with_256color "screen-256"
