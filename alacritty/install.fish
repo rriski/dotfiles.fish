86d04265d47eaef7ae5e4eec46526ac9bbbb6b7c
Timo Riski <github@timoriski.fi>
2023-05-18 23:24:42 +0300

    chore: remove alacritty

--- alacritty/install.fish
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env fish
-
-source $DOTFILES/script/utils.fish
-
-curl -sL https://raw.githubusercontent.com/dracula/alacritty/master/dracula.yml -o ~/.config/alacritty/dracula.yml
-
-link_file $DOTFILES_ROOT/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml backup
-	or abort alacritty
