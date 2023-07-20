3e64580323cb90d5e6ed11accfcca996413211de
Timo Riski <github@timoriski.fi>
2023-07-20 22:05:51 +0300

    fix(func): fix `pubkey` function on linux

--- ssh/functions/pubkey.fish
+++ ssh/functions/pubkey.fish
@@ -1,4 +1,10 @@
 function pubkey
-	more ~/.ssh/id_rsa.pub | pbcopy
-		and echo (set_color brblue)'-> Public key copied to clipboard'
+    set PUBKEY ~/.ssh/id_ed25519.pub
+    switch (uname)
+    case Darwin
+        more $PUBKEY | pbcopy
+		    and echo (set_color brblue)'-> Public key copied to clipboard'
+    case Linux
+        xclip -selection clipboard -i < $PUBKEY
+    end
 end
