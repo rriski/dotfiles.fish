9c3e16ff861b6e81579b244f4c4eaa0145abfe68
Timo Riski <github@timoriski.fi>
2023-07-20 21:45:53 +0300

    chore: reset nvim

--- nvim/init.lua
+++
@@ -1,6 +0,0 @@
-_G.LVIM_NVIM_VERSION = vim.version()
-if _G.LVIM_NVIM_VERSION.major == 0 and _G.LVIM_NVIM_VERSION.minor < 9 then
-    print("LVIM IDE required Neovim >= 0.9.0")
-else
-    require("core")
-end
--- nvim/lua/modules/base/configs/ui/heirline/buf_types.lua
+++
@@ -1,5 +0,0 @@
-return {
-    "nofile",
-    "prompt",
-    "help",
-}
