341c318662626e09c0368d4666f1beefc6cc2c00
Timo Riski <github@timoriski.fi>
2023-09-20 13:28:57 +0300

    brew: link Brewfile only if `brew` command exists

--- brew/install.fish
+++ brew/install.fish
@@ -2,5 +2,7 @@
 
 source $DOTFILES/script/utils.fish
 
-link_file $DOTFILES_ROOT/brew/Brewfile $HOME/Brewfile backup
-	or abort brew
+if command -q brew
+	link_file $DOTFILES_ROOT/brew/Brewfile $HOME/Brewfile backup
+	    or abort brew
+end
