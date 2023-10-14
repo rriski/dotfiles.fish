7590624a699333bcf295830ffad2d4dd7ff7f653
Timo Riski <github@timoriski.fi>
2023-10-15 02:39:58 +0300

    atuin: fix abort msg

--- atuin/install.fish
+++ atuin/install.fish
@@ -4,7 +4,7 @@ source $DOTFILES/script/utils.fish
 
 if command -q atuin
     link_file $DOTFILES_ROOT/atuin/config.toml $HOME/.config/atuin/config.toml backup
-	    or abort brew
+	    or abort atuin
 
     atuin gen-completions --shell fish > ~/.config/fish/completions/atuin.fish
 end
