fce79313bab5dd7cac0074dfb409b50620717d13
Timo Riski <github@timoriski.fi>
2023-10-15 18:09:20 +0300

    firefox: add user.js and tridactyl config

---
+++ firefox/user-overrides.js
@@ -0,0 +1,3 @@
+user_pref("browser.safebrowsing.downloads.remote.enabled", true); // Enable download lookups to Google Safe Browsing
+user_pref("privacy.clearOnShutdown.history", false); // Don't clear browsing and download history on shutdown
+user_pref("privacy.resistFingerprinting.letterboxing", false); // Don't use dynamic margin on websites
