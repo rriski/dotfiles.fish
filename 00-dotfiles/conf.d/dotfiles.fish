c1875da3b6130a2ebc58cc3292ce2b4e7ab385cc
Timo Riski <github@timoriski.fi>
2023-11-05 19:31:35 +0200

    chore(dotfiles): add brew sbin to `fish_user_paths`

--- 00-dotfiles/conf.d/dotfiles.fish
+++ 00-dotfiles/conf.d/dotfiles.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     if test (uname) = Darwin
-        fish_add_path -am (brew --prefix)/bin
+        fish_add_path -am (brew --prefix)/bin (brew --prefix)/sbin
     end
 
     fish_add_path -am $DOTFILES/bin $HOME/.bin $HOME/.local/bin
