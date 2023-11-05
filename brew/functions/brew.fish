baeac48883c03d82c6a351b59cc35b31f8a171cc
Timo Riski <github@timoriski.fi>
2023-11-05 19:26:01 +0200

    fix(brew): check for brew command

--- brew/functions/brew.fish
+++ brew/functions/brew.fish
@@ -1,12 +1,14 @@
 function brew -w brew
-	switch $argv[1]
-	case cleanup
-		# See $DOTFILES/bin/brew-cleanup
-		brew-cleanup
-	case bump
-		# See $DOTFILES/bin/brew-bump
-		brew-bump
-	case '*'
-		command brew $argv
-	end
+    switch $argv[1]
+    case cleanup
+        # See $DOTFILES/bin/brew-cleanup
+        brew-cleanup
+    case bump
+        # See $DOTFILES/bin/brew-bump
+        brew-bump
+    case '*'
+        if command -q brew
+            command brew $argv
+        end
+    end
 end
