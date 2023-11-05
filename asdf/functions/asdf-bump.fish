5226892bcc702f50619b378739f1fe7928d8d077
Timo Riski <github@timoriski.fi>
2023-11-05 22:32:14 +0200

    feat(asdf): add custom `asdf-prune` and `asdf-bump` functions

---
+++ asdf/functions/asdf-bump.fish
@@ -0,0 +1,11 @@
+function asdf-bump -d "bump all asdf tools to latest version"
+    for line in (asdf current)
+        set -l tool_name (echo $line | cut -d " " -f 1)[1]
+        set latest_version (asdf latest $tool_name)
+
+        if test $status -eq 0
+            asdf install $tool_name latest
+            asdf global $tool_name (asdf latest $tool_name)
+        end
+    end
+end
