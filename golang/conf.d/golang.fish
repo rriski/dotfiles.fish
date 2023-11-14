5a5f9d83b1093a3e52e5b967b4e7c0df0a08713c
Timo Riski <github@timoriski.fi>
2023-11-14 12:34:55 +0200

    go: fix GOROOT and GOPATH

--- golang/conf.d/golang.fish
+++ golang/conf.d/golang.fish
@@ -1,5 +1,8 @@
 if status is-interactive
 	if command -qs go
+        set -gx GO111MODULE on
+        set -gx GOROOT (asdf where golang)/go
+        set -gx GOPATH (asdf where golang)/packages
         fish_add_path -am (go env GOPATH)/bin
     end
 end
--- golang/install.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env fish
-
-if command -q go
-    set -Ux GO111MODULE on
-    set -Ux GOPATH (asdf where golang)/packages
-    set -Ux GOROOT (asdf where golang)/go
-end
