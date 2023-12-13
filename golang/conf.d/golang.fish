93fbc731aeaf243bbfe79acce51b1d5be0f7c3c2
Timo Riski <github@timoriski.fi>
2023-12-13 11:29:41 +0200

    go: set GOROOT and GOPATH only if installed with asdf

--- golang/conf.d/golang.fish
+++ golang/conf.d/golang.fish
@@ -1,5 +1,5 @@
 if status is-interactive
-	if command -qs go
+	if test -d (asdf where golang)
         set -gx GO111MODULE on
         set -gx GOROOT (asdf where golang)/go
         set -gx GOPATH (asdf where golang)/packages
