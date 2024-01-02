be89cdb3b7cabab30fcd5dcba0aa9aab5f862fb2
Timo Riski <github@timoriski.fi>
2024-01-02 14:01:14 +0200

    yabai: use `borders`
    https://github.com/FelixKratz/JankyBorders

--- yabai/install.fish
+++ yabai/install.fish
@@ -8,3 +8,6 @@ source $DOTFILES/script/utils.fish
 
 link_file $DOTFILES_ROOT/yabai/yabairc $HOME/.config/yabai/yabairc backup
 	or abort yabai
+
+link_file $DOTFILES_ROOT/yabai/bordersrc $HOME/.config/borders/bordersrc backup
+	or abort yabai
