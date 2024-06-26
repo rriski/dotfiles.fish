b901bd2bfb33b8eb73614ef1fa487295d5767ed0
Timo Riski <github@timoriski.fi>
2024-06-26 11:36:12 +0300

    fix: check if varclear exists before invoking it

--- 00-dotfiles/install.fish
+++ 00-dotfiles/install.fish
@@ -14,7 +14,9 @@ for f in $DOTFILES/*/functions
 	set -Up fish_function_path $f
 end
 
-varclear fish_function_path # Remove duplicate entries from fish_function_path
+if functions -q varclear
+    varclear fish_function_path # Remove duplicate entries from fish_function_path
+end
 
 for f in $DOTFILES/*/conf.d/*.fish
 	ln -sf $f $__fish_config_dir/conf.d/(basename $f)
